8bd3a89c60c4fdcc3ab71d532ecd2839269d0cfa
GmzAaaln <agomezj2101@alumno.ipn.mx>
2024-11-05 16:00:27 -0600

    //

--- Principal.java
+++ Principal.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-
+ //hola
 public class Principal {
 
     static boolean existenErrores = false;
